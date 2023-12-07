7201111c1bf80e7f122c4d8a97daffb37541c287
Tony-91 <73248515+Tony-91@users.noreply.github.com>
2023-12-07 15:58:49 -0800

    fnished setup

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import InstagramFeed from './components/InstagramFeed';
-import TwitterFeed from './components/TwitterFeed';
+import React from "react";
+import { Route, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import InstagramFeed from "./components/InstagramFeed";
+import TwitterFeed from "./components/TwitterFeed";
 
 function App() {
   return (
-    <Router>
+    <router>
       <div className="App">
         <Navbar />
-        <Switch>
-          <Route path="/instagram" component={InstagramFeed} />
-          <Route path="/twitter" component={TwitterFeed} />
-          <Route path="/" exact>
+        <routes>
+          <route path="/instagram" element={<InstagramFeed />}></route>
+          <route path="/twitter" element={<TwitterFeed />}></route>
+          <Route path="/">
             <h2>Home</h2>
             <p>Welcome to the Social Media Aggregator App!</p>
           </Route>
-        </Switch>
+        </routes>
       </div>
-    </Router>
-  );
-}
-
-export default App;
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </router>
   );
 }
 
