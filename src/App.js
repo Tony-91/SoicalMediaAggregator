1f9a938ddc5e194754dea8817790f88183ec8edd
Tony-91 <73248515+Tony-91@users.noreply.github.com>
2023-12-07 15:59:29 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 // src/App.js
 import React from "react";
-import { Route, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import InstagramFeed from "./components/InstagramFeed";
 import TwitterFeed from "./components/TwitterFeed";
 
 function App() {
   return (
-    <router>
+    <Router>
       <div className="App">
         <Navbar />
-        <routes>
-          <route path="/instagram" element={<InstagramFeed />}></route>
-          <route path="/twitter" element={<TwitterFeed />}></route>
-          <Route path="/">
-            <h2>Home</h2>
-            <p>Welcome to the Social Media Aggregator App!</p>
-          </Route>
-        </routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/instagram" element={<InstagramFeed />} />
+          <Route path="/twitter" element={<TwitterFeed />} />
+        </Routes>
       </div>
-    </router>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to the Social Media Aggregator App!</p>
+  </div>
+);
 
 export default App;
