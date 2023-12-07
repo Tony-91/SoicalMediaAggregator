58866aa39c2e740bbb0ccb46e6436e12a957dccf
Tony-91 <73248515+Tony-91@users.noreply.github.com>
2023-12-07 14:14:19 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import InstagramFeed from './components/InstagramFeed';
+import TwitterFeed from './components/TwitterFeed';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/instagram" component={InstagramFeed} />
+          <Route path="/twitter" component={TwitterFeed} />
+          <Route path="/" exact>
+            <h2>Home</h2>
+            <p>Welcome to the Social Media Aggregator App!</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 import logo from './logo.svg';
 import './App.css';
 
