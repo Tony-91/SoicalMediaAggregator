872bef26e65b9afb2903e5f4daa4193ea31ef973
Tony-91 <73248515+Tony-91@users.noreply.github.com>
2023-12-08 11:50:52 -0800

    layout progress

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import InstagramFeed from "./components/InstagramFeed";
 import TwitterFeed from "./components/TwitterFeed";
+// import "./styles/App.css"; // Import the CSS file for styling
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app-container">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -21,9 +22,9 @@ function App() {
 }
 
 const Home = () => (
-  <div>
-    <h2>Home</h2>
-    <p>Welcome to the Social Media Aggregator App!</p>
+  <div className="home-container">
+    <h2>Welcome to the Social Media Aggregator App!</h2>
+    <p>Effortlessly view your Instagram and Twitter feeds in one place.</p>
   </div>
 );
 
